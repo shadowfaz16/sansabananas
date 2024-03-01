9dc5f76f957f8ca3d89238d8a371de2e618cb078
shadowfaz16 <erikfazju@gmail.com>
2024-03-01 16:54:56 -0700

    stupid fix

--- pages/create/index.tsx
+++ pages/create/index.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Page, Input, Button, Copy, Nav } from "../../components";
 import { useCreateStream } from "@livepeer/react";
 import { Stream, CreateSignedPlaybackResponse } from "../../types";
-import Broadcast from "./broadcast";
+import Broadcast from "./Broadcast";
 import axios from "axios";
 import Link from "next/link";
 import toast from "react-hot-toast";
@@ -35,9 +35,9 @@ export default function Create() {
   } = useCreateStream(
     streamName
       ? {
-          name: streamName,
-          playbackPolicy: { type: "jwt" },
-        }
+        name: streamName,
+        playbackPolicy: { type: "jwt" },
+      }
       : null
   );
 
@@ -181,16 +181,15 @@ export default function Create() {
                 </Button>
 
                 <Button
-                  className={`bg-zinc-700 text-white px-5 py-3 mt-2 border-none  ml-6 ${
-                    !shareLink ? " cursor-not-allowed opacity-20 " : ""
-                  }`}
+                  className={`bg-zinc-700 text-white px-5 py-3 mt-2 border-none  ml-6 ${!shareLink ? " cursor-not-allowed opacity-20 " : ""
+                    }`}
                   text="text-md"
                   onClick={copy}
                 >
                   Copy link
                   <Copy text={shareLink} />
                 </Button>
-                <Broadcast/>
+                <Broadcast />
               </div>
             ) : (
               <div className="w-1/3 mt-20">
@@ -201,11 +200,10 @@ export default function Create() {
                 />
                 <div className="flex justify-end">
                   <Button
-                    className={`bg-primary border-primary text-background px-4 py-2.5 ${
-                      status === "loading" || !address || !streamName
+                    className={`bg-primary border-primary text-background px-4 py-2.5 ${status === "loading" || !address || !streamName
                         ? "cursor-not-allowed opacity-20"
                         : ""
-                    }`}
+                      }`}
                     text="text-sm"
                     onClick={() => createStream?.()}
                   >
